8fe3411ab1f569c0cc61b8487baed12f192f08bf
Rishi Karthikeyan <rishikarthikeyan@Rishis-MacBook-Air-2.local>
2022-02-06 23:19:07 +0400

    Login route created successfully

--- routes/auth.js
+++ routes/auth.js
@@ -6,17 +6,41 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const PASS_SEC = process.env.PASS_SEC;
+
 // REGISTER
 router.post('/register', async (req, res) => {
-  const userDetails = req.body;
+  const registerDetails = req.body;
   const newUser = new User({
-    username: userDetails.username,
-    email: userDetails.email,
-    password: CryptoJS.AES.encrypt(userDetails.password, PASS_SEC).toString(),
+    username: registerDetails.username,
+    email: registerDetails.email,
+    password: CryptoJS.AES.encrypt(
+      registerDetails.password,
+      PASS_SEC
+    ).toString(),
   });
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// Login
+router.post('/login', async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    if (!user) {
+      return res.status(401).json('Wrong credentials!');
+    }
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SEC
+    );
+
+    req.body.password == hashedPassword.toString(CryptoJS.enc.Utf8)
+      ? res.status(200).json(user)
+      : res.status(401).json('Wrong credentials!');
   } catch (error) {
     res.status(500).json(error);
   }
