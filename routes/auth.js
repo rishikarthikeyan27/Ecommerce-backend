6124040da242973cf43d2b5cf7ac47650274b01c
Rishi Karthikeyan <rishikarthikeyan@Rishis-MacBook-Air-2.local>
2022-02-06 23:48:44 +0400

    JWT implementation done

--- routes/auth.js
+++ routes/auth.js
@@ -41,14 +41,19 @@ router.post('/login', async (req, res) => {
     req.body.password != hashedPassword.toString(CryptoJS.enc.Utf8) &&
       res.status(401).json('Wrong credentials!');
 
-    const accessToken = jwt.sign({
-      id: user._id,
-      isAdmin: user.isAdmin,
-    });
+    const accessToken = jwt.sign(
+      {
+        id: user._id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SEC,
+      { expiresIn: '3d' }
+    );
+    console.log('AT: ', accessToken);
 
     const { password, ...others } = user._doc;
 
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (error) {
     res.status(500).json(error);
   }
