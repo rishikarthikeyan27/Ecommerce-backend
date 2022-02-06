a2b0ad88e1b51d317aa2f4caa50bba4e918e9542
Rishi Karthikeyan <rishikarthikeyan@Rishis-MacBook-Air-2.local>
2022-02-06 22:52:35 +0400

    register api ready

--- routes/auth.js
+++ routes/auth.js
@@ -1,13 +1,18 @@
 const router = require('express').Router();
 const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+const dotenv = require('dotenv');
 
+dotenv.config();
+
+const PASS_SEC = process.env.PASS_SEC;
 // REGISTER
 router.post('/register', async (req, res) => {
   const userDetails = req.body;
   const newUser = new User({
     username: userDetails.username,
     email: userDetails.email,
-    password: userDetails.password,
+    password: CryptoJS.AES.encrypt(userDetails.password, PASS_SEC).toString(),
   });
   try {
     const savedUser = await newUser.save();
