161f9412b8b1cbedf7325c952540cf9bcc47ca4d
Rishi Karthikeyan <rishikarthikeyan@Rishis-MacBook-Air-2.local>
2022-02-07 12:36:40 +0400

    Add product API created

---
+++ routes/product.js
@@ -0,0 +1,20 @@
+const router = require('express').Router();
+const Product = require('../models/Product');
+const {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+} = require('../middleware/verifyToken');
+
+// Create product
+router.post('/', verifyTokenAndAdmin, async (req, res) => {
+  const newProduct = new Product(req.body);
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+module.exports = router;
